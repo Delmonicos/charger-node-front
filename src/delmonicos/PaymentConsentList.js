870e2bfe5cfac1765e61372aba69dab51062ce87
Fabrice CROISEAUX <fcroiseaux@gmail.com>
2021-04-14 12:01:55 +0200

    Use Semantic UI Modal for Signature validation

--- src/delmonicos/PaymentConsentList.js
+++ src/delmonicos/PaymentConsentList.js
@@ -1,112 +1,134 @@
 import {Button, Table, Modal, Header, Icon, Form, Input, Grid} from 'semantic-ui-react';
-import { useState } from 'react';
+import {useState} from 'react';
 import {stringToU8a, u8aToHex, hexToU8a} from "@polkadot/util";
 
-export default function PaymentConsentList({ consents }) {
-  return (
-    <div>
-      <h1>Payment Consents</h1>
-      <Table celled striped size='small'>
-        <Table.Body>
-          <Table.Row>
-            <Table.Cell width={4}>
-              <strong>Address</strong>
-            </Table.Cell>
-            <Table.Cell width={8}>
-              <strong>Name</strong>
-            </Table.Cell>
-            <Table.Cell width={8}>
-            </Table.Cell>
-          </Table.Row>
-          { consents.map(user =>
-            <Table.Row key={user}>
-              <Table.Cell>
-                { user.address }
-              </Table.Cell>
-              <Table.Cell>
-                { user.name }
-              </Table.Cell>
-              <Table.Cell>
-                <VerifySignature consent={user}/>
-              </Table.Cell>
-            </Table.Row>
-          )}
-        </Table.Body>
-      </Table>
-    </div>
-  );
+export default function PaymentConsentList({consents}) {
+    return (
+        <div>
+            <h1>Payment Consents</h1>
+            <Table celled striped size='small'>
+                <Table.Body>
+                    <Table.Row>
+                        <Table.Cell width={4}>
+                            <strong>Address</strong>
+                        </Table.Cell>
+                        <Table.Cell width={8}>
+                            <strong>Name</strong>
+                        </Table.Cell>
+                        <Table.Cell width={8}>
+                        </Table.Cell>
+                    </Table.Row>
+                    {consents.map(user =>
+                        <Table.Row key={user}>
+                            <Table.Cell>
+                                {user.address}
+                            </Table.Cell>
+                            <Table.Cell>
+                                {user.name}
+                            </Table.Cell>
+                            <Table.Cell>
+                                <VerifySignature consent={user}/>
+                            </Table.Cell>
+                        </Table.Row>
+                    )}
+                </Table.Body>
+            </Table>
+        </div>
+    );
 };
 
-const VerifySignature= ({consent}) => {
-  const [open, setOpen] = useState(false)
-  const [bic, setBic] = useState('');
-  const [iban, setIban] = useState('');
-  const [verificationResult, setVerificationResult] = useState('');
+const VerifySignature = ({consent}) => {
+    const [open, setOpen] = useState(false)
+    const [bic, setBic] = useState('');
+    const [iban, setIban] = useState('');
+    const [iconColor, setIconColor] = useState('white');
+    const [iconName, setIconName] = useState('none');
 
-  const validate = () => {
-    const message = bic + iban;
-    const signature = hexToU8a(consent.sig);
-    const keypairs = keyring.getPairs();
-    const account = keypairs.find((k) => k.address === consent.address);
-    const isValid = account.verify(message, signature);
-    setVerificationResult('Résultat : ' + isValid);
-  };
+    const validate = () => {
+        const message = bic + iban;
+        const signature = hexToU8a(consent.sig);
+        const keypairs = keyring.getPairs();
+        const account = keypairs.find((k) => k.address === consent.address);
+        if (account.verify(message, signature)) {
+            showResultIcon(1);
+        } else {
+            showResultIcon(-1);
+        }
+    };
 
-  const openDialog= () => {
-    setBic('');
-    setIban('');
-    setVerificationResult('');
-    setOpen(true);
-  };
+    const openDialog = () => {
+        setBic('');
+        setIban('');
+        showResultIcon(0);
+        setOpen(true);
+    };
 
-  return (
-      <Modal
-          onClose={() => setOpen(false)}
-          onOpen={() => openDialog()}
-          open={open}
-          trigger={<Button>Validate</Button>}
-      >
-        <Modal.Header>Consent Verification for {consent.name}</Modal.Header>
-        <Modal.Content>
-          <p>
-            Stored signature : <small>{consent.sig}</small>
-          </p>
-          <Modal.Description>
-            <Header>Enter the information you want to verify</Header>
-            <p/>
-          </Modal.Description>
-          <Form>
-            <Form.Field>
-              <Input
-                  placeholder='BIC'
-                  value={bic}
-                  onChange={(_,{ value }) => setBic(value)}
-              />
-            </Form.Field>
-            <Form.Field>
-              <Input
-                  placeholder='IBAN'
-                  value={iban}
-                  onChange={(_,{ value }) => setIban(value)}
-              />
-            </Form.Field>
-          </Form>
-          <p>
-            {verificationResult}
-          </p>
-        </Modal.Content>
-        <Modal.Actions>
-          <Button
-              content="Validate"
-              labelPosition='right'
-              icon='checkmark'
-              onClick={() => validate()}
-              positive
-          />
-          <Button color='red' onClick={() => setOpen(false)}>
-            Close
-          </Button>
-        </Modal.Actions>
-      </Modal>
-  );
+    const showResultIcon = (state) => {
+        switch (state) {
+            case -1:
+                setIconColor('red');
+                setIconName('thumbs down');
+                break;
+            case 1:
+                setIconColor('green');
+                setIconName('thumbs up');
+                break;
+            default:
+                setIconColor('white');
+                setIconName('');
+        }
+    }
+
+    return (
+        <Modal
+            onClose={() => setOpen(false)}
+            onOpen={() => openDialog()}
+            open={open}
+            trigger={<Button>Validate</Button>}
+        >
+            <Modal.Header>Consent Verification for {consent.name}</Modal.Header>
+            <Modal.Content>
+                <p>
+                    Stored signature : <small>{consent.sig}</small>
+                </p>
+                <Modal.Description>
+                    <Header>Enter the information you want to verify</Header>
+                    <p/>
+                </Modal.Description>
+                <div style={{display: 'flex', flexDirection: 'row'}}>
+                    <Form>
+                        <Form.Field>
+                            <Input
+                                placeholder='BIC'
+                                value={bic}
+                                onChange={(_, {value}) => setBic(value)}
+                            />
+                        </Form.Field>
+                        <Form.Field>
+                            <Input
+                                placeholder='IBAN'
+                                value={iban}
+                                onChange={(_, {value}) => setIban(value)}
+                            />
+                        </Form.Field>
+                    </Form>
+                    <div style={{width: '100%'}}>
+                        <Icon style={{display: 'block', marginRight: 'auto', marginLeft: 'auto'}} color={iconColor} name={iconName} size='huge'/>
+                    </div>
+                </div>
+            </Modal.Content>
+            <Modal.Actions>
+                <Button
+                    content="Validate"
+                    labelPosition='right'
+                    icon='checkmark'
+                    onClick={() => validate()}
+                    positive
+                />
+                <Button color='red' onClick={() => setOpen(false)}>
+                    Close
+                </Button>
+            </Modal.Actions>
+        </Modal>
+    );
 };
